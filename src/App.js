1b2975c2b3c19600bcc7aaf454cd54117db93f5d
Mark Pearyer <Marcusp619@gmail.com>
2017-11-10 21:01:30 -0500

    added controlled input

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-submit = () => {
-    console.log(this.text.value)
+  state = {
+    input: 'Hello'
   }
+
+  updateInput = (event) => {
+    this.setState({
+      input: event.target.value
+    })
+  }
+  
+  submit = () => {
+      console.log(this.text.value)
+    }
 
   render() {
     return (
@@ -17,6 +27,7 @@ submit = () => {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="text" onChange={this.updateInput} value= {this.state.input} />
         <input type="text" ref={(input) => this.text = input }/>
         <button onClick={this.submit}>Show Value</button>
       </div>
