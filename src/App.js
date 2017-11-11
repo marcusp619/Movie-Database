92446f283f674441606d1782e14b7d3ed6085150
Mark Pearyer <Marcusp619@gmail.com>
2017-11-10 21:36:31 -0500

    Added new component w/ map func

--- src/App.js
+++ src/App.js
@@ -2,45 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie'
+
+const movies = [{
+  id: 1,
+  title: 'Star Wars'
+  }, {
+  id: 2,
+  title: 'Spider Man'
+  }, {
+  id: 3,
+  title: '36th Chamber of Death'
+  }
+];
+
+
 class App extends Component {
-  state = {
-    input: 'Hello'
-  }
-
-  updateInput = (event) => {
-    this.setState({
-      input: event.target.value
-    })
-  }
-  
-  submit = () => {
-      console.log(this.text.value)
-    }
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React" />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input type="text" onChange={this.updateInput} value= {this.state.input} />
-        <input type="text" ref={(input) => this.text = input }/>
-        <button onClick={this.submit}>Show Value</button>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
-  }
-}
-
-class Welcome extends Component {
-  render() {
-    const { text } = this.props;
-    return (
-      <h1 className="App-title">{text}</h1>
-    )
   }
 }
 
