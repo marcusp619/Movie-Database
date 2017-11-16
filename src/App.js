8cca2a44f6ab5227b3c49ee93c3713996a41978a
Mark Pearyer <Marcusp619@gmail.com>
2017-11-15 22:01:24 -0500

    Added dynamic routes using params

--- src/App.js
+++ src/App.js
@@ -3,48 +3,32 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
+  Link,
 } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-import Movie from './Movie';
+import MoviesList from './MoviesList';
 
-class App extends Component {
-  state = {
-    movies: [],
-  }
-
-
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=5cb4969ffac5d2d967beb6c31aa53509&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Route path="/test" component={Test} />
-          {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
 
-const Test = () => (
-  <h1>Test</h1>
+const Test = ({ match }) => (
+  <h1>{match.params.id}</h1>
 );
